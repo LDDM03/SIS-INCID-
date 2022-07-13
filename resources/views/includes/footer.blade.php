d0e51cb4e32b422fddf469f21d414e2be2028a21
Luis Diaz <luisdiaz03022017@gmail.com>
2022-07-13 09:38:31 -0400

    VIsta del footer de todo el sistema

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,7 +1,7 @@
 
     <div class="text-center p-3" style="background-color: #20374c; margin-top: -30px">
       © 2022 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/"
+      <a class="text-white" href="https://github.com/LDDM03"
          >Luis Diaz</a
         >
     </div>
